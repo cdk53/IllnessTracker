e40d5bc0d7d1978dd9d885c877d68b5d64b12bd5
CameronKnaus <cdk53@zips.uakron.edu>
2019-10-23 13:08:00 -0400

    Add another route example to routes.js:

--- src/routes.js
+++ src/routes.js
@@ -38,6 +38,21 @@ app.get('/illnesses', function (req, res) {
     });
   });
 });
+// Returns all symptom1 from illness table
+app.get('/symptomtest', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query('SELECT symptom1 FROM symptoms', function (error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
 
 // Starting our server.
 app.listen(3000, () => {
